98260753be407841d6626e59ab218d582ffb92c9
Ammar_Emad <za0663008@gmail.com>
2026-05-03 22:06:30 +0300

    feat: implement onboarding screen UI with goal selection and availability preferences

--- lib/features/onboarding/ui/screens/onboarding_screen.dart
+++ lib/features/onboarding/ui/screens/onboarding_screen.dart
@@ -92,8 +92,7 @@ class OnboardingScreen extends StatelessWidget {
                     },
                   ),
                 ],
-              )
-           
+              ),
     
-  }
+      
 }
