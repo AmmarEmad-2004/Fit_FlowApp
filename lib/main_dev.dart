bfc9b394b31849ab10275bd86d2c1de8ee1a5d09
Ammar_Emad <za0663008@gmail.com>
2026-05-08 16:00:56 +0300

    feat: implement environment-specific entry points and refactor app initialization

---
+++ lib/main_dev.dart
@@ -0,0 +1,9 @@
+import 'package:fit_flow/core/config/app_config.dart';
+import 'package:fit_flow/run_app.dart';
+
+void main() {
+  AppConfig appConfig = const AppConfig(environment: AppEnvironment.development);
+  runFitApp(
+     appConfig
+  );
+}
--- lib/run_app.dart
+++ lib/run_app.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_flow/core/config/app_config.dart';
+import 'package:fit_flow/core/di/service_locator.dart';
+import 'package:fit_flow/firebase_options.dart';
 import 'package:fit_flow/fit_flow_app.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'core/di/service_locator.dart';
-import 'firebase_options.dart';
-
-Future<void> main() async {
+void runFitApp(AppConfig appConfig) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  getIt.registerLazySingleton<AppConfig>(() => appConfig);
   await Hive.initFlutter();
   await setupGetIt();
   runApp(const FitFlowApp());
