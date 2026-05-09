7932b9c54003adcd751d80d62fa7d7cceecf1625
Ammar_Emad <za0663008@gmail.com>
2026-05-09 22:28:43 +0300

    feat: add WorkoutProgramModel and define HomeRepo interface for fetching programs

--- lib/features/home/data/models/workout_program_model.dart
+++ lib/features/home/data/models/workout_program_model.dart
@@ -1,16 +1,15 @@
 import 'training_day_model.dart';
 
 class WorkoutProgramModel {
+  
+
+  final String programType;
+  final int daysPerWeek;
+
+  final List<TrainingDayModel> trainingDays;
   const WorkoutProgramModel({
     required this.programType,
     required this.daysPerWeek,
     required this.trainingDays,
   });
-
-  /// e.g. "muscle", "strong", "general"
-  final String programType;
-  final int daysPerWeek;
-
-  /// All training days for this program, ordered by day index
-  final List<TrainingDayModel> trainingDays;
 }
