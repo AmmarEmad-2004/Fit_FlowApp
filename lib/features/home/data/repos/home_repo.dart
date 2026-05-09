52336435a285cb7dd8a89807555a5dd327161873
Ammar_Emad <za0663008@gmail.com>
2026-05-09 22:28:50 +0300

    feat: create HomeRepo abstract class for workout program data retrieval

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,6 +1,5 @@
 import '../../data/models/workout_program_model.dart';
 
 abstract class HomeRepo {
-  /// Fetches the full workout program (all training days + exercises) at once.
   Future<WorkoutProgramModel> getProgram();
 }
