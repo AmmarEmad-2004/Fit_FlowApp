c5c5353e208a8434f4038fcb61263b7fcd629583
Ammar_Emad <za0663008@gmail.com>
2026-05-13 23:45:46 +0300

    feat: implement onboarding flow with cubit state management and splash screen navigation

--- lib/features/onboarding/data/repos/onboarding_repo.dart
+++ lib/features/onboarding/data/repos/onboarding_repo.dart
@@ -3,5 +3,8 @@ import '../models/onboarding_model.dart';
 abstract class OnboardingRepo {
   Future<OnboardingModel> getOnboardingData();
 
-  void persistSelection({required String goalId, required String availability});
+  Future<void> persistSelection({
+    required String goalId,
+    required String availability,
+  });
 }
