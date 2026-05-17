8320579d87a7ec7c5c748302276564fcbdc0aa1e
Ammar_Emad <za0663008@gmail.com>
2026-05-17 23:22:57 +0300

    feat: implement repository and cubit layers for onboarding and home features

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,8 +1,5 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/failures/failure.dart';
 import '../../data/models/workout_program_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, WorkoutProgramModel>> getProgram();
+  Future<WorkoutProgramModel> getProgram();
 }
