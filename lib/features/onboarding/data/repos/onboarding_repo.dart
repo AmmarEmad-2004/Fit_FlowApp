e63a7ff8635accb6078b8e98daf52c282ba96d35
Ammar_Emad <za0663008@gmail.com>
2026-05-17 23:25:04 +0300

    feat: implement onboarding cubit and repository for state management and data persistence

--- lib/features/onboarding/data/repos/onboarding_repo.dart
+++ lib/features/onboarding/data/repos/onboarding_repo.dart
@@ -1,10 +1,7 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/failures/failure.dart';
 import '../models/onboarding_model.dart';
 
 abstract class OnboardingRepo {
-  Future<Either<Failure, OnboardingModel>> getOnboardingData();
+  Future<OnboardingModel> getOnboardingData();
 
-  Either<Failure, Unit> persistSelection({required String goalId, required String availability});
+  void persistSelection({required String goalId, required String availability});
 }
