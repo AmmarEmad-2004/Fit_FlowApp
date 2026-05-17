b9822947a3c9792b9434cc77cad2b5673f885664
Ammar_Emad <za0663008@gmail.com>
2026-05-17 23:15:44 +0300

    feat: implement onboarding and home feature layers with repository and cubit patterns

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/failures/error_handler.dart';
+import '../../../../core/failures/failure.dart';
 import '../models/workout_program_model.dart';
 import '../services/home_service.dart';
 import 'home_repo.dart';
@@ -8,5 +12,12 @@ class HomeRepoImpl implements HomeRepo {
   final HomeService _service;
 
   @override
-  Future<WorkoutProgramModel> getProgram() => _service.loadProgram();
+  Future<Either<Failure, WorkoutProgramModel>> getProgram() async {
+    try {
+      final program = await _service.loadProgram();
+      return Right(program);
+    } catch (e) {
+      return Left(ErrorHandler.handle(e));
+    }
+  }
 }
